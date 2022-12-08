2e95d10b64e7f1f9cf0572e57d0538323360764e
davodyadegari <davoud.yadegari88@gmail.com>
2022-12-08 21:21:33 +0330

    complete AccountService

--- src/main/java/com/example/sample/service/AccountService.java
+++ src/main/java/com/example/sample/service/AccountService.java
@@ -3,11 +3,19 @@ package com.example.sample.service;
 import com.example.sample.entity.Account;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AccountService {
+    Account login(String cardNumber, String password);
+
     void save(Account account);
 
     List<Account> findAll();
+
+//    Optional<Account> findById(Integer id);
+//
+//    void delet(Account account);
+
 
 //    List<Account> getAccounts();
 }
