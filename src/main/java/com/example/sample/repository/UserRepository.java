4940dd23899ecd649a55fa6f4abc6a1261c2e240
davodyadegari <davoud.yadegari88@gmail.com>
2022-12-08 20:31:01 +0330

    complete UserRepository

--- src/main/java/com/example/sample/repository/UserRepository.java
+++ src/main/java/com/example/sample/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.sample.repository;
 import com.example.sample.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User>findByNationalCode(String nationalCode);
 }
