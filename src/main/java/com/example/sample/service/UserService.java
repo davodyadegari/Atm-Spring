094bd428ad8ec13831f4f8f6d0b8da6edfea8b79
davodyadegari <davoud.yadegari88@gmail.com>
2022-12-08 21:47:14 +0330

    complete UserService

--- src/main/java/com/example/sample/service/UserService.java
+++ src/main/java/com/example/sample/service/UserService.java
@@ -2,6 +2,12 @@ package com.example.sample.service;
 
 import com.example.sample.entity.User;
 
+import java.time.LocalDate;
+
 public interface UserService {
+    User createUser(String name, String family, String nationalCode, LocalDate birthDay);
+
+    User findAccountByNationalCode(String nationalCode);
+
     void save(User user);
 }
