68c3f0fff103dc9d0ba4662b2d116658926f118c
davodyadegari <davoud.yadegari88@gmail.com>
2022-12-08 20:42:30 +0330

    complete UserRepository

--- src/main/java/com/example/sample/repository/TransactionRepository.java
+++ src/main/java/com/example/sample/repository/TransactionRepository.java
@@ -1,9 +1,12 @@
 package com.example.sample.repository;
 
 import com.example.sample.entity.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+    Page<Transaction>findByAccount_Card_CardNumberOrderByDate(String cardNumber, Pageable pageable);
 }
