93377d732baba7a506b16ef7a7b26e30043a0f5c
davodyadegari <davoud.yadegari88@gmail.com>
2022-12-08 20:25:18 +0330

    complete AccountRepository

--- src/main/java/com/example/sample/repository/AccountRepository.java
+++ src/main/java/com/example/sample/repository/AccountRepository.java
@@ -1,9 +1,16 @@
 package com.example.sample.repository;
 
 import com.example.sample.entity.Account;
+import com.example.sample.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account,Long> {
+   Optional<Account>findByCard_CardNumber(String cardNumber);
+
 }
